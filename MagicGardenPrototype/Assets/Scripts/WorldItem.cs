60e87b7272af00ba8c88f9c58f5c1d18f830ba6a
jackwil93 <mail.jackwilson@gmail.com>
2018-09-17 21:58:19 +1000

    Moving items and saving + loading their positions and objects now works! Woop!

---
+++ MagicGardenPrototype/Assets/Scripts/WorldItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldItem : MonoBehaviour { // Must be MonoBehaviour so it can exist in the scene
+
+    public string itemID;
+    public string displayedName;
+    public int potID;
+    public int plantID;
+    public float ageTime;
+    public int invSlotNumber = 999;
+    public bool inWorld = true;
+    public string placedPointName;
+    public float placedPointX;
+    public float placedPointY;
+    public float placedPointZ;
+
+    public void SpawnSelf(GameObject pot, GameObject plant) // Called from GameManager
+    {
+        GameObject myPot = Instantiate(pot, this.transform);
+        GameObject myPlant = Instantiate(plant, myPot.transform.Find("node_flower_base"));
+
+        BoxCollider boxCol = this.gameObject.AddComponent<BoxCollider>();
+        boxCol.center = new Vector3(0, 0.44f, 0);
+
+        transform.tag = "moveable";
+        transform.position = new Vector3(placedPointX, placedPointY, placedPointZ);
+
+        if (itemID != null)
+            this.name = itemID;
+        else
+            this.name = "Plant Item";
+    }
+}
